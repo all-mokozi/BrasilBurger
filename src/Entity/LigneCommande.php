bb2f98423eef56e11f14473247a9bbc00a8ac710
Mohamed Maiga <mohamed.maiga@ism.edu.sn>
2025-12-30 23:32:14 +0000

    probleme

--- src/Entity/LigneCommande.php
+++ src/Entity/LigneCommande.php
@@ -2,7 +2,7 @@
 
 namespace App\Entity;
 
-use App\Repository\LigneCommandeRepository;
+use App\Repository\Impl\LigneCommandeRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
--- src/Repository/Impl/LigneCommandeRepository.php
+++ src/Repository/Impl/LigneCommandeRepository.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Repository;
+namespace App\Repository\Impl;
 
 use App\Entity\LigneCommande;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
